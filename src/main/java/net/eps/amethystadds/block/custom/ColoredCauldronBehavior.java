a39ea9634a58b1f083be1ad64550b732c8a15748
Lon4th <jsag6996@gmail.com>
2023-01-07 11:11:16 +0000

    Deleted unnecessary files

--- src/main/java/net/eps/amethystadds/block/custom/ColoredCauldronBehavior.java
+++ src/main/java/net/eps/amethystadds/block/custom/ColoredCauldronBehavior.java
@@ -1,26 +1,16 @@
 package net.eps.amethystadds.block.custom;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectFunction;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.eps.amethystadds.AmethystAdditions;
-import net.eps.amethystadds.block.ModBlocks;
 import net.minecraft.block.*;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.potion.PotionUtil;
-import net.minecraft.potion.Potions;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public interface ColoredCauldronBehavior extends CauldronBehavior {
@@ -43,7 +33,7 @@ public interface ColoredCauldronBehavior extends CauldronBehavior {
     };
 
     public static Object2ObjectOpenHashMap<Item, ColoredCauldronBehavior> createMap() {
-        return Util.make(new Object2ObjectOpenHashMap(), map -> map.defaultReturnValue((state, world, pos, player, hand, stack) -> ActionResult.PASS));
+        return Util.make(new Object2ObjectOpenHashMap<>(), map -> map.defaultReturnValue((state, world, pos, player, hand, stack) -> ActionResult.PASS));
     }
 
     public ActionResult interact(BlockState var1, World var2, BlockPos var3, PlayerEntity var4, Hand var5, ItemStack var6);
